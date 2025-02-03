5bc3a4c3adaf2b03ec53836f3135cdeb89b6aeb3
Andrey Lushnikov <aslushnikov@gmail.com>
2025-02-03 00:57:57 -0700

    staging

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+import pagefind from 'astro-pagefind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    format: "file",
+  },
+  integrations: [preact(), pagefind()]
+});
