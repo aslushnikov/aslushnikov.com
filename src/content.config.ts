4961a6a73917ac35f99b5f89b1f892e467986bcd
Andrey Lushnikov <aslushnikov@gmail.com>
2025-02-03 18:12:52 -0700

    feat: add "backpacking" folder

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,8 +3,8 @@ import { glob } from "astro/loaders";
 // Import utilities from `astro:content`
 import { defineCollection, z } from "astro:content";
 // Define a `loader` and `schema` for each collection
-const blog = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.md', base: "./src/blog" }),
+const backpacking = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.md', base: "./src/backpacking" }),
   schema: ({ image }) => z.object({
     title: z.string(),
     since: z.date(),
@@ -23,4 +23,4 @@ const blog = defineCollection({
   })
 });
 // Export a single `collections` object to register your collection(s)
-export const collections = { blog };
+export const collections = { backpacking };
